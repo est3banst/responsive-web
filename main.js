f8d903c43e891dc8721d9a551eec1fbf7cf7a824
est3banst <estebeeen@outlook.com>
2023-08-06 02:23:26 -0300

    adding js to small resolution pics

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-const furniture = document.querySelector('.furniture-designs')
-const imgs = gallery.querySelectorAll('.work-gallery')
+const furniture = document.querySelector('.furniture-designs');
+const imgs = furniture.querySelectorAll('.work-gallery');
+// const decksand = document.querySelector('.decks-perg-designs');
+// const imgs2 = decksand.querySelectorAll('.gallery-works'); 
+const next = document.getElementById('next');
+const prev = document.getElementById('prev');
+
+
+let currentImage = 0
+
+function slideNext() {
+    currentImage += 1
+    console.log(imgs[currentImage])
+}
+
+function slidePrev() {
+    currentImage -= 1
+    imgs[currentImage]
+}
+
+next.addEventListener('click', () => {
+    slideNext()
+    console.log("one")
+    // e.preventDefault();
+});
+
 
