1f32d0b01722e593128e1dfedbb22e8bebdcf3ea
est3banst <estebeeen@outlook.com>
2023-08-07 01:25:38 -0300

    modifying js file, making img slider

--- main.js
+++ main.js
@@ -1,27 +1,13 @@
-const furniture = document.querySelector('.furniture-designs');
-const imgs = furniture.querySelectorAll('.work-gallery');
-// const decksand = document.querySelector('.decks-perg-designs');
-// const imgs2 = decksand.querySelectorAll('.gallery-works'); 
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
+// const furniture = document.querySelector('.furniture-designs');
+// const imgs = furniture.querySelectorAll('.work-gallery');
+// // const decksand = document.querySelector('.decks-perg-designs');
+// // const imgs2 = decksand.querySelectorAll('.gallery-works'); 
+// const next = document.getElementById('next');
+// const prev = document.getElementById('prev');
 
-
-let currentImage = 0
-
-function slideNext() {
-    currentImage += 1
-    console.log(imgs[currentImage])
-}
-
-function slidePrev() {
-    currentImage -= 1
-    imgs[currentImage]
-}
-
-next.addEventListener('click', () => {
-    slideNext()
-    console.log("one")
-    // e.preventDefault();
-});
-
-
+// furniture.onmouseover = (e) => {
+//     furniture.dataset.mouseDownAt = e.clientX
+// }
+// next.addEventListener('click',() => {
+//     furniture.style.translate = '-20%';
+// })
