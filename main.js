8c85f49a10c52f49a04ccb7e3457eafdf96c43cf
est3banst <estebeeen@outlook.com>
2023-08-09 03:34:31 -0300

    making some changes..

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
-// const furniture = document.querySelector('.furniture-designs');
-// const imgs = furniture.querySelectorAll('.work-gallery');
-// // const decksand = document.querySelector('.decks-perg-designs');
-// // const imgs2 = decksand.querySelectorAll('.gallery-works'); 
-// const next = document.getElementById('next');
-// const prev = document.getElementById('prev');
+const form = document.getElementById('my-form');
+const button = document.querySelector('.contact-now');
 
-// furniture.onmouseover = (e) => {
-//     furniture.dataset.mouseDownAt = e.clientX
-// }
-// next.addEventListener('click',() => {
-//     furniture.style.translate = '-20%';
-// })
+button.addEventListener('click', ()=> {
+    form.style.display = 'block'
+    form.style.margin = 'auto'
+
+});
+
+// button.addEventListener('click', function() {
+//     if (form.style.display = 'flex') {
+//         form.style.display = 'none'
+//     }
+    
+// });
