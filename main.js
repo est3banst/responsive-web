490c44205717edd78ac6caefb30bea0e810d1c9e
est3banst <estebeeen@outlook.com>
2023-08-10 04:17:52 -0300

    making more changes

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const form = document.getElementById('my-form');
-const button = document.querySelector('.contact-now');
-
-button.addEventListener('click', ()=> {
-    form.style.display = 'block'
-    form.style.margin = 'auto'
-
-});
-
-// button.addEventListener('click', function() {
-//     if (form.style.display = 'flex') {
-//         form.style.display = 'none'
-//     }
-    
-// });
+const observer = new IntersectionObserver(entries => {
+    console.log(entries)
+})
